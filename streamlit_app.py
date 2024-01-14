ce5248246df5b8663427d7f0bacd0fecdeaaf30c
miyabiit <yamada.masahiro@gmail.com>
2024-01-15 08:54:28 +0900

    dataset show

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,15 @@ from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 import plotly.graph_objects as go
 
-test = 3 + 5
-test
+st.title("主成分分析 Wine価格")
+
+dataset = load_wine()
+df = pd.DataFrame(dataset.data)
+df.columns = dataset.feature_names
+df.target = dataset.target
+
+if st.checkbox("Wineデータセットの詳細"):
+    dataset.DESCR
+if st.checkbox("Wineデータセット閲覧"):
+    df
+
