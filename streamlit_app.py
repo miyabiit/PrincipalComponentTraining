c5b7dc6b31483582bc712106fef2ae26562d801b
miyabiit <yamada.masahiro@gmail.com>
2024-01-15 08:43:18 +0900

    modules import

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_wine
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+import plotly.graph_objects as go
+
+test = 3 + 5
+test
