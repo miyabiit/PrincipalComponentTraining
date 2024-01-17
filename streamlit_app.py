49d8762e91bdac65bf396c7229dd880b4414281e
miyabiit <yamada.masahiro@gmail.com>
2024-01-17 09:22:22 +0900

    pca add

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,31 @@ st.title("主成分分析 Wine価格")
 dataset = load_wine()
 df = pd.DataFrame(dataset.data)
 df.columns = dataset.feature_names
-df.target = dataset.target
+df["target"] = dataset.target
 
 if st.checkbox("Wineデータセットの詳細"):
     dataset.DESCR
 if st.checkbox("Wineデータセット閲覧"):
     df
 
+# standard
+x = df.drop(columns = ["target"])
+y = df["target"]
+
+sscaler = StandardScaler()
+x_std = sscaler.fit_transform(x)
+
+st.sidebar.markdown(
+        r"""
+        ### number of Pricipal Factors
+        """
+        )
+num_pca = st.sidebar.number_input(
+        "Minimum Value",
+        value = 3,
+        step = 1,
+        min_value = 3
+        )
+
+pca = PCA(n_components = num_pca)
+pca.fit_transform(x_std)
